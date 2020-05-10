f083a07445b5050a46b8c5769b699037ce4dedbf
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-10 19:25:29 +0430

    implement printColumnsName (first line of print)

--- index.js
+++ index.js
@@ -2,5 +2,28 @@ const fs = require('fs').promises;
 
 (async file => {
   const content = await fs.readFile(file);
+
   console.log('content:', content);
 })(process.argv[2]);
+
+function display(content) {
+  printColumnsName();
+  printRecords(content);
+}
+
+function printColumnsName() {
+  let rowNames = '\t\t';
+  // For Hex columns
+  for(let i = 0; i < 16; i++) {
+    rowNames += i.toString(16).padStart(2, '0') + ' ';
+    if(i % 4 === 3) {
+      rowNames += ' '.repeat(2);
+    } 
+  }
+  // For character columns
+  rowNames += '\t';
+  for(let i = 0; i < 16; i++) {
+    rowNames += i.toString(16);
+  }
+  return rowNames;
+}
