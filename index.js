1b71df53a587a1634c383ab8fe84d87a73641ce0
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-11 01:27:27 +0430

    accept a string value as input or a file path

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const fs = require('fs').promises;
 const prettyDisplay = require('./lib/pretty-display');
 (async file => {
-  const content = await fs.readFile(file);
+  let content = '';
+  try {
+    const fileStat = await fs.lstat(file);
+    if(fileStat.isFile()) {
+      content = await fs.readFile(file);
+    }
+  } catch(err) {
+    content = Buffer.from(file);
+  }
 
   prettyDisplay(content);
 
