a5329ee2c16011e263c695c88fbd03c49cbfa121
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-11 02:21:43 +0430

    add feature to be albe to read from stdin stream for using pipe character '|'

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const fs = require('fs').promises;
 const prettyDisplay = require('./lib/pretty-display');
+
 (async file => {
-  let content = '';
   try {
+
     const fileStat = await fs.lstat(file);
     if(fileStat.isFile()) {
-      content = await fs.readFile(file);
+      const content = await fs.readFile(file);
+      prettyDisplay(content);
+    } else {
+      throw new Error('The input is not a file');
     }
-  } catch(err) {
-    content = Buffer.from(file);
-  }
 
-  prettyDisplay(content);
+  } catch(err) {
+
+    if(file) {
+      const content = Buffer.from(file);
+      prettyDisplay(content);
+    } else {
+      let content = Buffer.from('');
+      process.stdin.on('data', function(chunk) {
+        content = Buffer.concat([content, chunk]);
+      });
+      process.stdin.on('end', function() {
+        prettyDisplay(content);
+      });
+    }
+
+  }
 
 })(process.argv[2]);
 
